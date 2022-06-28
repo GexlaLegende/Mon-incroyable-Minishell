eb5b636afaec6061258cdf275f20527cab742262
apercebo <apercebo@student.42lyon.fr>
2022-06-28 15:43:32 +0200

    signals

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: apercebo <apercebo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 14:26:18 by apercebo          #+#    #+#             */
-/*   Updated: 2022/06/28 10:51:55 by apercebo         ###   ########.fr       */
+/*   Updated: 2022/06/28 12:06:07 by apercebo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@
 # include <signal.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <termios.h>
 
 typedef struct s_cmd_list
 {
