2c6ac69494b956ab84dbbf3118368833d94a98c7
apercebo <apercebo@student.42lyon.fr>
2022-06-28 16:43:25 +0200

    Here_doc var added

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: apercebo <apercebo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 14:26:16 by apercebo          #+#    #+#             */
-/*   Updated: 2022/06/28 15:44:54 by apercebo         ###   ########.fr       */
+/*   Updated: 2022/06/28 16:41:53 by apercebo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,18 +29,19 @@ int	str_is_empty(char *str)
 	return (0);
 }
 
-static void	handler(int sigtype)
+/* static void	handler(int sigtype)
 {
 	if (sigtype == SIGINT)
 	{
-		write(1, "\n", 1);
+		if (!g_signal_flags)
+			printf("\n");
 		rl_on_new_line();
 		rl_replace_line("", 0);
 		rl_redisplay();
 	}
-}
+} */
 
-void setup_term(int	save)
+/* void setup_term(int	save)
 {
     static struct termios t;
 	static int	ifsave;
@@ -58,14 +59,12 @@ void setup_term(int	save)
 	   tcsetattr(STDOUT_FILENO, TCSANOW, &saved);
 	else
 	   tcsetattr(STDOUT_FILENO, TCSANOW, &t);
-}
+} */
 
 int	main(int argc, char **argv, char **env)
 {
 	t_data				data;
 
-	signal(SIGINT, handler); // ctrl-C
-	signal(SIGQUIT, SIG_IGN); // ctrl-backslash */
 	if (argc != 1 || !(argv[0]))
 		exit (0);
 	data.paths = recup_path(env, &data);
@@ -74,7 +73,9 @@ int	main(int argc, char **argv, char **env)
 	{
 		data.cmd_table = ft_lstnew(NULL, NULL, NULL);
 		data.here_doc_nbr = 0;
-		setup_term(0);
+		//setup_term(0);
+		/* signal(SIGINT, handler); // ctrl-C
+		signal(SIGQUIT, SIG_IGN); // ctrl-backslash */
 		data.main_str = readline("Minishell $> ");
 		if (str_is_empty(data.main_str) != 0)
 			add_history(data.main_str);
@@ -86,7 +87,6 @@ int	main(int argc, char **argv, char **env)
 			parserror(ft_env_var(&data));
 			exekerror(ft_execution(&data, env));
 		}
-		setup_term(1);
 		free(data.main_str);
 		if (data.main_error != -1)
 			ft_lstclear(&data.cmd_table_temp);
