15151014c679835b0295abb5873583f039bba711
Delphine Bouron <dbouron@student.42lyon.fr>
2022-06-28 11:22:28 +0200

    unset norm OK

--- sources/bin_unset.c
+++ sources/bin_unset.c
@@ -6,7 +6,7 @@
 /*   By: dbouron <dbouron@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 07:06:56 by apercebo          #+#    #+#             */
-/*   Updated: 2022/06/28 10:24:38 by dbouron          ###   ########lyon.fr   */
+/*   Updated: 2022/06/28 11:16:19 by dbouron          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,45 +14,24 @@
 
 void	bin_unset(char **arg, t_data *data)
 {
-	int			i;
-	int			j;
-	char		*name;
-	t_env_list	*save_p_next;
-	t_env_list	*p_env_name;
-
-	j = 0;
-	while (arg[j])
+	data->f = 0;
+	while (arg[data->f])
 	{
-		i = 0;
-		while (arg[j][i])
+		data->e = 0;
+		while (arg[data->f][data->e])
 		{
-			while (ft_isalnum(arg[j][i]) == 1 || arg[j][i] == '_')
-				i++;
-			if ((arg[j][i] == '=' || arg[j][i] == '\0') && i > 0)
-			{
-				name = ft_substr(arg[j], 0, i);
-				p_env_name = ft_search_preenv(data, name);
-				if (!p_env_name)
-					return ;
-				else
-				{
-					save_p_next = p_env_name->next;
-					p_env_name->next = p_env_name->next->next;
-					save_p_next->next = NULL;
-					ft_env_lstclear(&save_p_next);
-				}
-			}
+			while (ft_isalnum(arg[data->f][data->e]) == 1 \
+				|| arg[data->f][data->e] == '_')
+				data->e++;
+			if ((arg[data->f][data->e] == '=' || arg[data->f][data->e] == '\0') \
+				&& data->e > 0)
+				ft_remove_env_var(arg, data);
 			else
-			{
-				while (arg[j][i] && arg[j][i] != ' ')
-					i++;
-				name = ft_substr(arg[j], 0, i);
-				printf("minishell: unset: `%s': not a valid identifier\n", name);
-			}
-			if (arg[j][i])
-				i++;
+				ft_name_error(arg, data);
+			if (arg[data->f][data->e])
+				data->e++;
 		}
-		j++;
+		data->f++;
 	}
 }
 
@@ -68,4 +47,33 @@ t_env_list	*ft_search_preenv(t_data *data, char *name)
 		current_elmt = current_elmt->next;
 	}
 	return (NULL);
+}
+
+void	ft_remove_env_var(char **arg, t_data *data)
+{
+	char		*name;
+	t_env_list	*p_env_name;
+	t_env_list	*save_p_next;
+
+	name = ft_substr(arg[data->f], 0, data->e);
+	p_env_name = ft_search_preenv(data, name);
+	if (!p_env_name)
+		return ;
+	else
+	{
+		save_p_next = p_env_name->next;
+		p_env_name->next = p_env_name->next->next;
+		save_p_next->next = NULL;
+		ft_env_lstclear(&save_p_next);
+	}
+}
+
+void	ft_name_error(char **arg, t_data *data)
+{
+	char	*name;
+
+	while (arg[data->f][data->e] && arg[data->f][data->e] != ' ')
+		data->e++;
+	name = ft_substr(arg[data->f], 0, data->e);
+	printf("minishell: unset: `%s': not a valid identifier\n", name);
 }
